f707a65663314942e0be1ca518b25d06c44bcbf4
Eric and Brian <EricandBrian@appacademy.io>
2013-04-01 10:52:41 -0700

    Create Visit model

--- URLShortener/app/models/user.rb
+++ URLShortener/app/models/user.rb
@@ -1,4 +1,5 @@
 class User < ActiveRecord::Base
   has_many :links
   has_many :comments
+  has_many :visits
 end
--- URLShortener/db/schema.rb
+++ URLShortener/db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20130401172837) do
+ActiveRecord::Schema.define(:version => 20130401174652) do
 
   create_table "comments", :force => true do |t|
     t.text     "body"
@@ -32,6 +32,13 @@ ActiveRecord::Schema.define(:version => 20130401172837) do
   create_table "users", :force => true do |t|
     t.string   "username"
     t.string   "email"
+    t.datetime "created_at", :null => false
+    t.datetime "updated_at", :null => false
+  end
+
+  create_table "visits", :force => true do |t|
+    t.integer  "user_id"
+    t.integer  "link_id"
     t.datetime "created_at", :null => false
     t.datetime "updated_at", :null => false
   end
